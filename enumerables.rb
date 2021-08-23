524eef025c54aaf9089ab977fedd2d8fa6076cba
kw-8 <kwu11235813@gmail.com>
2021-08-23 12:21:41 -0700

    up to my_flatten

--- enumerables.rb
+++ enumerables.rb
@@ -13,4 +13,31 @@ class Array
         self.my_each{ |ele| new_array << ele if prc.call(ele)}
         new_array
     end
+
+    def my_reject(&prc)
+        new_array = []
+        self.my_each{ |ele| new_array << ele unless prc.call(ele)}
+        new_array
+    end
+
+    def my_any?(&prc)
+        self.my_each{|el| return true if prc.call(el)}
+        false
+    end
+
+    def my_all?(&prc)
+        self.my_each{|el| return false unless prc.call(el)}
+        true
+    end
+
+    def my_flatten
+        new_array = []
+        self.my_each do |el|
+            if el.class == Array
+                new_array += el.my_flatten
+            else new_array += [el]
+            end
+        end
+        new_array
+    end
 end
